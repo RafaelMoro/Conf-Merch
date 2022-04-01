9b35cb8ba1f1dce8345daf7ab61a28bc5a9f694b
Rafael Moro <rafael.moro.galindo@gmail.com>
2022-04-01 01:09:11 -0600

    [ADD] Sass and Css plugins and loaders were installed. the app now supports Css and SCSS

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
 module.exports = {
     entry: './src/index.js',
@@ -25,6 +26,14 @@ module.exports = {
                 use: [
                     { loader: 'html-loader' }
                 ]
+            },
+            {
+                test: /\.(scss|css)$/,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                    'sass-loader'
+                ]
             }
         ]
     },
@@ -32,6 +41,9 @@ module.exports = {
         new HtmlWebpackPlugin({
             template: './public/index.html',
             filename: './index.html'
+        }),
+        new MiniCssExtractPlugin({
+            filename: '[name].css'
         })
     ]
 }
