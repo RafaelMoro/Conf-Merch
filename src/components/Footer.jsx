b0664158af70eb9f3ab380f8408ce8ef93033389
Rafael Moro <rafael.moro.galindo@gmail.com>
2022-04-03 13:44:12 -0500

    [ADD] - The footer styles has been added

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
-
+import '@styles/components/Footer.scss'
 const Footer = () => {
     return (
-        <div>
-            Footer
-        </div>
+        <footer className='footer'>
+            <div className='left-title'>
+                <p>Platzi Conference Merch</p>
+            </div>
+            <div className='right-title'>
+                <p>Todos los izquierdos reservados</p>
+            </div>
+        </footer>
     );
 };
 
