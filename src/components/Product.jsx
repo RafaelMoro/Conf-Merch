cfadb671cb55ef40b2e09d46b4cfc3440baa7a68
Rafael Moro <rafael.moro.galindo@gmail.com>
2022-04-11 01:32:11 -0500

    [FIX] The grid issue of not adjusting to different screens was fixed. The pay button was added with some styles and was centered

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -11,6 +11,7 @@ const Product = ({product}) => {
                 <h3 className='product__title'>{product.title}</h3> <p className='product__price'>${product.price}</p>
             </div>
             <p className='product__description'>{product.description}</p>
+            <button className='product__buy-button'>Comprar</button>
         </article>
     );
 };
