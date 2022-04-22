6054b26eb3c6cce0937638e0962240b2a37f82ef
Rafael Moro <rafael.moro.galindo@gmail.com>
2022-04-21 23:19:29 -0500

    Success component has been coded completely along with their css styles. leaflet and react-leaflet were installed and it works properly.

--- src/pages/Success.jsx
+++ src/pages/Success.jsx
@@ -1,8 +1,26 @@
 import React from 'react'
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
+import '@styles/pages/Success.scss'
 
 const Success = () => {
     return(
-        <h1>Success</h1>
+        <main className='success'>
+            <h1 className='success__title'>Gracias por tu compra.</h1>
+            <p className='success__description'>Tu pedido llegará a su domicilio en 3 días.</p>
+            <div className='success__map'>
+                <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
+                    <TileLayer
+                        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                    />
+                    <Marker position={[51.505, -0.09]}>
+                        <Popup>
+                        A pretty CSS3 popup. <br /> Easily customizable.
+                        </Popup>
+                    </Marker>
+                </MapContainer>
+            </div>
+        </main>
     )
 }
 export {Success}
